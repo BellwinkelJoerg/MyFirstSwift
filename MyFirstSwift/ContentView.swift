49ad296e142e2b2cad07742935a99b14523012c2
Jörg Bellwinkel <joerg@bellwinkel.de>
2020-04-13 17:48:54 +0200

    Latest Version on 2020-04-13

--- MyFirstSwift/ContentView.swift
+++ MyFirstSwift/ContentView.swift
@@ -12,12 +12,22 @@ import SwiftUI
 struct ContentView: View {
 
     let colors: [Color] = [.red, .green, .blue]
+//    let Solved: Int = 0
+    let Total: Int = 0
 //    let value1: Int = 0
 //    let value2: Int = 0
     @State private var Value1 = ""
     @State private var Value2 = ""
+//    @State private int Solved
+
+
     
-    
+    var Result: Int{
+        let myVal1 = Int(Value1) ?? 0
+        let myVal2 = Int(Value2) ?? 0
+        let Total = myVal1 + myVal2
+        return Total
+    }
     
     var body: some View {
         VStack {
@@ -28,7 +38,7 @@ struct ContentView: View {
 
             Form{
                 Section {
-                    TextField("Eingabe Value1: ", text: $Value1)
+                    TextField("Eingabe Wert 1: ", text: $Value1)
                 }
 
                 Section {
@@ -38,7 +48,7 @@ struct ContentView: View {
 
             Form{
                 Section {
-                    TextField("Eingabe Value2: ", text: $Value2)
+                    TextField("Eingabe Wert 2: ", text: $Value2)
                 }
 
                 Section {
@@ -47,6 +57,11 @@ struct ContentView: View {
             }
 
             
+//            Form{
+//                Section {
+                    Text("Ergebnis: $\(Total)")
+//                }
+//            }
             
             
             ForEach(colors, id: \.self) { color in
@@ -55,7 +70,7 @@ struct ContentView: View {
                     .background(color)
             }
 
-        }.frame(minWidth:300, maxWidth: 600, minHeight: 200, maxHeight: 400)
+        }.frame(minWidth:300, maxWidth: .infinity, minHeight: 200, maxHeight: .infinity)
     }
 
         
