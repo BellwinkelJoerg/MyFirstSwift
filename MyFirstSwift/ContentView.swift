4e8d05a9687332243efd37b818108ae820f27d18
Jörg Bellwinkel <joerg@bellwinkel.de>
2020-04-13 11:49:35 +0200

    Initial Commit

---
+++ MyFirstSwift/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MyFirstSwift
+//
+//  Created by Jörg Bellwinkel on 13.04.20.
+//  Copyright © 2020 Jörg Bellwinkel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
